18c3795ff66978b145e0cf0f48546133a405b73c
Jake McCann <mccann.307@buckeyemail.osu.edu>
2022-07-24 13:26:06 -0400

    small fix

--- stock_app/app/models/user.rb
+++ stock_app/app/models/user.rb
@@ -12,9 +12,10 @@ class User < ApplicationRecord
   # Updates the value of users account when stock price updates
   # old_price: num_stock_owned * pre_update_stock_price
   # new_price: num_stock_owned * post_update_stock_price
+  # user_id: id of user whole value is being updated
   def self.price_update user_id, old_price, new_price
     user = User.where(:id => user_id).first
-    user.currentbalance += new_price = old_price
+    user.currentbalance += (new_price - old_price)
     user.save
   end
 end
