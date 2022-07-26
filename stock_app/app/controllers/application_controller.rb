83bc47f123f44519f05617c90d623bb57e534e83
moonnoah <moon.503@osu.edu>
2022-07-26 11:58:37 -0400

    added inital balance field for signup, setup logic, reroute login and signup pages to stocks index

--- stock_app/app/controllers/application_controller.rb
+++ stock_app/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :add_signup_params, if: :devise_controller?
+
+  protected
+
+  # Created 7/26/22 by Noah Moon
+  def add_signup_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:liquidcash])
+  end
 end
